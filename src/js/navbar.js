d9f7869ccf25f83e6de8f358812eb2e47794d17e
kevinkurian7 <kevinthathathil@gmail.com>
2022-07-19 23:11:33 +0530

    q

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -4,7 +4,7 @@ nav.innerHTML = `
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 <a class="navbar-brand" href="/">
-  <h1>BlockVote<h1>
+  <img src="./img/eci.png" width="150px" loading="lazy"/>
 </a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
